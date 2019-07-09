eb65db955b7aed1a8d1ad117189e417e45b2e4ba
kly001 <karenly1961@gmail.com>
2019-07-08 23:39:04 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -31,41 +31,40 @@ server.get("/api/projects", async (req, res) => {
 
   // list a project by id
 server.get("/api/projects/:id", async (req, res) => {
-    try {
-      const project = await db("projects")
-        .where({ id: req.params.id })
-        .first();
-      res.status(200).json(project);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+  try {
+    const project = await db("projects")
+      .where({ id: req.params.id })
+      .first()
+      const action = await db("actions")
+      .where({ project_id: req.params.id })
+      .first()
+    res.status(200).json({...project,action});
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+server.get("/api/actions", async (req, res) => {
+  try {
+    const actions = await db("actions")
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  //list actions for a project
-  // server.get("/api/projects/:id/actions", async (req, res) => {
-  //   try {
-  //     const projectactions = await db("actions")
-  //       .where({ project_id: req.params.id })
-  //       .first();
-  //     res.status(200).json(actions);
-  //   } catch (error) {
-  //     res.status(500).json(error);
-  //   }
-  // });
+// list a project by id with actions - DOESN'T WORK
+server.get("/api/actions/:id/", async (req, res) => {
+  try {
+    const actions = await db("actions")
+      .where({ id: req.params.id })
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  server.get("/api/projects/:id/actions", async (req, res) => {
-    const {project_id} = req.params.id;
-    try {
-      const actions = await db(project_id);
-      if(actions.length) {
-        res.json(actions);
-      } else {
-        res.status(404).json({error:"No Actions found"})
-      }
-    } catch(error) {
-      res.status(500).json(error)
-    }
-  })
+  
+  
 
   //create a new project
   server.post("/api/projects", async (req, res) => {
@@ -80,27 +79,17 @@ server.get("/api/projects/:id", async (req, res) => {
       res.status(500).json({ message, error });
     }
   });
+
+  //create a new action
+  server.post("/api/actions", async (req, res) => {
+    try {
+      const action = await db("actions").insert(req.body)
+      res.status(201).json(action);
+    } catch (error) {
+      res.status(500).json({ message, error });
+    }
+  });
   
-  //update a project
-  // server.put("/api/projects/:id", async (req, res) => {
-  //   try {
-  //     const count = await db("projects")
-  //       .where({ id: req.params.id })
-  //       .update(req.body);
-  
-  //     if (count > 0) {
-  //       const project = await db("projects")
-  //         .where({ id: req.params.id })
-  //         .first();
-  
-  //       res.status(200).json(project);
-  //     } else {
-  //       res.status(404).json({ message: "The project was not found" });
-  //     }
-  //   } catch (error) {
-  // res.status(500).json({ message, error });
-  //}
-  // });
   
   // remove a project
   server.delete("/api/projects/:id", async (req, res) => {
